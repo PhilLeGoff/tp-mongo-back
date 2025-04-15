f8b5a11bf0b96ed55b1ca3c2dd3e2b29801bf7f3
Gloireski <belemgloirebekoutou8@gmail.com>
2025-04-15 15:12:00 +0200

    db conn

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,28 @@
-from flask import Flask
-from flask import request
+import configparser
+import os
+
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import certifi
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+config = configparser.ConfigParser()
+config.read(os.path.abspath(os.path.join(".ini")))
 
+# Configuration MongoDB
+app.config["MONGO_URI"] = config['PROD']['DB_URI']
+# Initialize PyMongo with SSL certificate validation
+mongo = PyMongo(app, tlsCAFile=certifi.where())
 @app.route('/')
-def hello_world():  # put application's code here
+def home():  # put application's code here
     return 'Hello World!'
 
+@app.route("/get", methods=["GET"])
+def get_data():
+    data = list(mongo.db.movies.find({}, {"_id": 0}).limit(50))
+    return jsonify(data), 200
 
 if __name__ == '__main__':
     app.run()
