3db4480fb184d80501e9dfc1bd59e025950713a4
Linnaelle <perret.clecle380@gmail.com>
2025-04-18 13:23:55 +0200

    Changements .env

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 from app import create_app
 import certifi
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Ensure SSL certificates are loaded
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv("FLASK_RUN_HOST", "127.0.0.1"),
+        port=int(os.getenv("FLASK_RUN_PORT", 5000)),
+        debug=True
+    )
